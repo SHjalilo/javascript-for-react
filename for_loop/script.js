1515e5c46616bcc22d006e9ba7d37e793f35a1d3
SHjalilo <aaaabdjalil@gmail.com>
2023-04-12 23:34:40 +0000

    syntax of : for ... in / for ... of

--- for_loop/script.js
+++ for_loop/script.js
@@ -16,7 +16,7 @@ for (var n =0; n <= 500; n++){
 
 //
 
-
+// show in html : 10 X 0 = 0 => 10 X 1 = 10 => 10 X 2 = 20 ...  
 for(var mul = 0; mul <=30; mul++){
 	var total = 10*mul;
 	console.log("10 X "+mul+" = "+total);
@@ -25,6 +25,41 @@ for(var mul = 0; mul <=30; mul++){
 	
 
 }
+
+// for loop with string 
+
+var nm = "jalil";
+// use '<' with 'length' => i=0 
+for(var i =0;i<nm.length;i++){
+	console.log(nm[i]);  // \n: j => a => l => i => l
+}
+
+// for ... in 
+console.log("---------");
+for(var i in nm){
+	console.log(i); // \n: 0 => 1 => 2 => 3 => 4
+}
+
+console.log("---------");
+
+for(var i in nm){
+	console.log(nm[i]); // \n: j => a => l => i => l
+}
+
+
+// for ... of 
+console.log("---------");
+
+for(var i of nm){
+	console.log(i); // \n: j => a => l => i => l 
+}
+
+
+
+
+
+
+
 
 
 
