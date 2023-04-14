de4c18cd8a4ffaa1d37a26d8c0437176a4f4907e
SHjalilo <aaaabdjalil@gmail.com>
2023-04-14 02:23:06 +0000

    continue done!

--- break_continue/script.js
+++ break_continue/script.js
@@ -13,9 +13,30 @@ var name = document.getElementById("std").value.toLowerCase();
     var tlc = student.toLowerCase();
     if(name == tlc ){
         document.getElementById("existStd").innerHTML = `${name} exists in students `;
-        break;
+        break; // break the loop => get out from loop
     }else{
         document.getElementById("existStd").innerHTML = `${name} is not in students `;
     }
   }
 }  
+//------------------------------------------------------------------------------------
+
+for(var i =0;i<sList.length;i++){
+document.getElementById("stdList2").innerHTML+=sList[i]+" - ";
+}
+var btn = document.getElementById("btnSearch2");
+btn.addEventListener("click",showFunction2);
+document.getElementById("existStd2").innerHTML = ` `;
+function showFunction2(){
+var name = document.getElementById("std2").value.toLowerCase(); 
+   for(var student of sList){
+    var tlc = student.toLowerCase();
+    document.getElementById("existStd2").innerHTML = `${name} is not in students `;
+    if(name != tlc ){
+	continue;
+    }else{
+        document.getElementById("existStd2").innerHTML = `${name} exists in students `;
+        break; 
+    }
+  }
+}
