f6013a83f2e444fc8af13b329054cda7d6c8eaa2
SHjalilo <aaaabdjalil@gmail.com>
2023-04-15 01:20:50 +0000

    nested loop

---
+++ nested_loop/script.js
@@ -0,0 +1,19 @@
+// nested loop
+
+
+var student = [
+["ahmed","ayoub","akram"],
+["med","mayara","mustafa"],
+["sido","said","salama"]
+]; 
+
+for(var i =0;i<student.length;i++){
+    var currentClass = student[i];
+    document.getElementById("std").innerHTML +=`<hr> Class ${i+1} : <hr>`;
+	for(var j=0;j<currentClass.length;j++){
+	    //console.log(currentClass[j]);
+	    //console.log(student[i][j]);
+	      var currentStudent = currentClass[j];
+	      document.getElementById("std").innerHTML +=` => ${currentStudent} <br>`;
+	}
+}
