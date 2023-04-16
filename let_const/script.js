9bae3babef7c193b48f22971908308a9c7959bc3
SHjalilo <aaaabdjalil@gmail.com>
2023-04-16 21:43:23 +0000

    done!

---
+++ let_const/script.js
@@ -0,0 +1,34 @@
+// let / const 
+
+// let 
+
+{
+let xyz = "hello";
+}
+
+console.log(xyz); // undefind
+
+function test(){
+    let x = 10;
+}
+
+console.log(x); // undefind
+
+let w = 10;
+w = 20 ;
+console.log(w); // 20
+
+// const 
+
+const numberOfMonths = 12 ;
+numberOfMonths = 19;
+
+console.log(numberOfMonths); // undefind
+
+
+{
+const y = "hello";
+}
+
+console.log(y); // undefind
+
