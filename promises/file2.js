ab00b970551d858ed866121e9b3d7be9d1deb7d5
SHjalilo <aaaabdjalil@gmail.com>
2023-05-06 02:38:14 +0100

    second commit

---
+++ promises/file2.js
@@ -0,0 +1,45 @@
+//
+
+let p = new Promise(function (resolve,reject) {
+  console.log("starting the async code !");
+  // async code
+  isSuccess = true;
+  if(isSuccess){
+    console.log("calling resolve !");
+    resolve();
+  }else {
+    console.log("calling reject !");
+    reject();
+  }
+});
+
+console.log("after the promis creation ! and befor then and catch ... ");
+
+p.then(function () {
+  console.log("calling then !");
+}).catch(function () {
+  console.log("error");
+});
+
+console.log("after then and catch !");
+
+//
+
+
+let promis = new Promise(function (resolve,reject) {
+  console.log("starting the async code");
+  // async code
+  setTimeout(function () {
+    document.getElementById('title1').style.visibility="visible";
+    resolve();
+    console.log("calling resolve");
+  },2000);
+
+});
+console.log("after the promis creation and befor then and catch");
+p.then(function () {
+  console.log("calling then !");
+}).catch(function () {
+  console.log("error");
+});
+console.log("after then and catch");
